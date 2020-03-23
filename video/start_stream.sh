331e5a95c082cf0b14f128b906cea413e5a73b1f
Ivo Ursino <ursinoivo@gmail.com>
2020-03-23 04:47:39 -0300

    Start VLC http stream server in background

--- video/start_stream.sh
+++ video/start_stream.sh
@@ -3,4 +3,4 @@
 VIDEO_DEVICE=$1
 PORT=$2
 
-vlc v4l2:///dev/$VIDEO_DEVICE --sout "#transcode{vcodec=mjpg}:std{access=http,mux=mpjpeg,dst=0.0.0.0:$PORT}"
+screen -d -m -S myVLCscreen vlc v4l2:///dev/$VIDEO_DEVICE --sout "#transcode{vcodec=mjpg}:std{access=http,mux=mpjpeg,dst=0.0.0.0:$PORT}"
